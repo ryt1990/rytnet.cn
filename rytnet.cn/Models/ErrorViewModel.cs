f9546034f8851d3e50ca6f4eee329b29a501ac61
任余涛 <renyutao1990@gmail.com>
2020-01-10 14:01:14 +0800

    项目初始化

---
+++ rytnet.cn/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace rytnet.cn.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
